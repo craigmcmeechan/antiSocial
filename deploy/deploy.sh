187a80fe8130ca79e87d476bdc6be4f272684c8e
Michael Rhodes <mrhodes@mediapolis.com>
2017-12-02 20:35:10 -0500

    .

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -16,7 +16,7 @@ run_grunt() {
 
 build_images() {
 	echo "docker-compose"
-	cd docker-assets
+	cd ~/repo/deploy/docker-assets
 	docker-compose build
 	docker tag webapp-antisocial:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/anti-social-$CIRCLE_BRANCH:webapp-antisocial
 	docker tag nginx-antisocial:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/anti-social-$CIRCLE_BRANCH:nginx-antisocial
@@ -29,13 +29,11 @@ build_images() {
 
 	echo "docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/anti-social-$CIRCLE_BRANCH:nginx-antisocial"
 	docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/anti-social-$CIRCLE_BRANCH:nginx-antisocial
-
-	cd ../
 }
 
 deploy_eb () {
 	if [ "$CIRCLE_BRANCH" == "production" ]; then
-		cd production
+		cd ~/repo/deploy/production
 
 		echo "deploying mr"
 		~/.local/bin/eb use mr-antisocial
@@ -44,15 +42,13 @@ deploy_eb () {
 		echo "deploying ae"
 		~/.local/bin/eb use ae-antisocial
 		~/.local/bin/eb deploy
-		cd ../
 	fi
 	if [ "$CIRCLE_BRANCH" == "development" ]; then
-		cd development
+		cd ~/repo/deploy/development
 
 		echo "deploying development"
 		~/.local/bin/eb use devel-antisocial
 		~/.local/bin/eb deploy
-		cd ../
 	fi
 }
 
