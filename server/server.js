6becf4f974fff6c57b5df29687544ad7a09c65a0
Michael Rhodes <mrhodes@mediapolis.com>
2018-01-07 18:14:11 -0400

    .

--- server/server.js
+++ server/server.js
@@ -167,18 +167,17 @@ app.use(function (req, res, next) {
 var csp = require('helmet-csp')
 
 app.use(csp({
-  // Specify directives as normal.
   'directives': {
     'defaultSrc': ['\'self\''],
-    'scriptSrc': ['\'self\'', "maps.googleapis.com", "csi.gstatic.com", "cdn.ravenjs.com", function (req, res) {
-      return "'nonce-" + app.locals.nonce + "'"
+    'scriptSrc': ['\'self\'', 'maps.googleapis.com', 'csi.gstatic.com', 'cdn.ravenjs.com', function (req, res) {
+      return '\'nonce-' + app.locals.nonce + '\'';
     }],
-    'fontSrc': ['\'self\'', "fonts.googleapis.com", "fonts.gstatic.com"],
-    'styleSrc': ['\'self\'', "fonts.googleapis.com", "'unsafe-inline'"],
-    'imgSrc': ['\'self\'', "csi.gstatic.com", "s3.amazonaws.com"],
+    'fontSrc': ['\'self\'', 'fonts.googleapis.com', 'fonts.gstatic.com'],
+    'styleSrc': ['\'self\'', 'fonts.googleapis.com', '\'unsafe-inline\''],
+    'imgSrc': ['\'self\'', 'csi.gstatic.com', 's3.amazonaws.com'],
     'sandbox': ['allow-forms', 'allow-scripts', 'allow-same-origin'],
     'reportUri': '/csp-violation',
-    'objectSrc': ["'none'"],
+    'objectSrc': ['\'none\''],
     'upgradeInsecureRequests': false
   },
   'loose': false,
