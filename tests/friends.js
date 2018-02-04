d2da0ae17fbe5f33013040e16353086ce1484b2c
Michael Rhodes <mrhodes@mediapolis.com>
2018-02-04 15:51:36 -0400

    fix tests

--- tests/friends.js
+++ tests/friends.js
@@ -15,11 +15,10 @@ describe('friends', function () {
 	var email1 = 'mrhodes+test1@myantisocial.net';
 	var email2 = 'mrhodes+test2@myantisocial.net';
 	var password = 'testing123';
-	var endpoint1 = 'http://127.0.0.1:3000/user-1';
-	var endpoint2 = 'http://127.0.0.1:3000/user-2';
-	var endpoint3 = 'http://127.0.0.1:3000/user-999';
+	var endpoint1 = 'http://127.0.0.1:3000/';
+	var endpoint2 = 'http://127.0.0.1:3000/';
+	var endpoint3 = 'http://127.0.0.1:3000/sdfsdfsdf';
 	var newEndPoint = 'http://127.0.0.1:3000/user-changed';
-	var theUser = null;
 
 	before(function (done) {
 		app.start();
@@ -31,7 +30,7 @@ describe('friends', function () {
 			'email': email1
 		},  function (err, info) {
 			expect(err).to.be(null);
-			console.log(info);
+			//console.log(info);
 			done();
 		});
 	});
@@ -41,7 +40,7 @@ describe('friends', function () {
 			'email': email2
 		},  function (err, info) {
 			expect(err).to.be(null);
-			console.log(info);
+			//console.log(info);
 			done();
 		});
 	});
@@ -49,7 +48,7 @@ describe('friends', function () {
 	it('delete all access tokens', function (done) {
 		app.models.AccessToken.destroyAll({},  function (err, info) {
 			expect(err).to.be(null);
-			console.log(info);
+			//console.log(info);
 			done();
 		});
 	});
@@ -58,7 +57,7 @@ describe('friends', function () {
 		app.models.Friend.destroyAll({
 			'remoteEndPoint': endpoint1
 		},  function (err, info) {
-			console.log(info);
+			//console.log(info);
 			expect(err).to.be(null);
 			done();
 		});
@@ -79,32 +78,17 @@ describe('friends', function () {
 			.type('form')
 			.send({
 				'email': email1,
-				'password': password
+				'password': password,
+				'name': 'user-1'
 			})
 			.end(function (err, res) {
 				expect(err).to.be(null);
 				expect(res.status).to.equal(200);
 				var accessToken = getCookie(res.headers['set-cookie'], 'access_token');
 				expect(accessToken).to.be.a('string');
-				app.models.MyUser.findOne({
-					'where': {
-						'email': email1
-					}
-				}, function (err, user) {
-					theUser = user;
-				});
+				endpoint1 += res.body.result.username;
 				done();
 			});
-	});
-
-	it('should be able to update user1', function (done) {
-		client1.patch('http://127.0.0.1:3000/api/MyUsers/me').send({
-			username: 'user-1',
-			name: 'User One'
-		}).end(function (err, res) {
-			expect(res.status).to.be(200);
-			done();
-		});
 	});
 
 	it('should be able to create account 2', function (done) {
@@ -112,25 +96,17 @@ describe('friends', function () {
 			.type('form')
 			.send({
 				'email': email2,
-				'password': password
+				'password': password,
+				'name': 'user-2'
 			})
 			.end(function (err, res) {
 				expect(err).to.be(null);
 				expect(res.status).to.equal(200);
 				var accessToken = getCookie(res.headers['set-cookie'], 'access_token');
 				expect(accessToken).to.be.a('string');
+				endpoint2 += res.body.result.username;
 				done();
 			});
-	});
-
-	it('should be able to update user2', function (done) {
-		client2.patch('http://127.0.0.1:3000/api/MyUsers/me').send({
-			username: 'user-2',
-			name: 'User Two'
-		}).end(function (err, res) {
-			expect(res.status).to.be(200);
-			done();
-		});
 	});
 
 	it('user1 should be able to friend user2', function (done) {
@@ -164,35 +140,6 @@ describe('friends', function () {
 			done();
 		});
 	});
-
-	/*
-		it('user1 should be able to change endpoint', function (done) {
-			app.models.Friend.find({
-				'where': {
-					'userId': theUser.id
-				}
-			}, function (err, friends) {
-				async.forEach(friends, function (friend, cb) {
-					var payload = {
-						'accessToken': friend.remoteAccessToken,
-						'newEndPoint': newEndPoint
-					}
-
-					console.log(payload);
-
-					request.post(friend.remoteEndPoint + '/friend-webhook/change-address').send(payload).end(function (err, res) {
-						expect(res.status).to.be(200);
-						expect(res.body.status).to.equal('ok');
-						cb();
-					});
-				}, function (err) {
-					done();
-				});
-			});
-
-		});
-
-	*/
 });
 
 function getCookie(headers, id) {
