c205a907129e4eec9b052779f78085f76491bf4d
Michael Rhodes <mrhodes@mediapolis.com>
2018-02-08 23:14:56 -0400

    simplify templates

--- server/server.js
+++ server/server.js
@@ -184,6 +184,7 @@ app.use(csp({
     }],
     'fontSrc': ['\'self\'', 'fonts.googleapis.com', 'fonts.gstatic.com'],
     'styleSrc': ['\'self\'', 'fonts.googleapis.com', '\'unsafe-inline\''],
+    'frameSrc': ['\'self\'', 'www.youtube.com'],
     'imgSrc': ['\'self\'', 'data:', 'csi.gstatic.com', 's3.amazonaws.com', 'maps.googleapis.com'],
     'sandbox': ['allow-forms', 'allow-scripts', 'allow-same-origin', 'allow-popups'],
     'reportUri': '/csp-violation',
