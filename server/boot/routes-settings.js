43deec97e6335d82c33fc4be632a9ac30e467b3b
Michael Rhodes <mrhodes@mediapolis.com>
2018-02-18 10:00:15 -0300

    .

--- server/boot/routes-settings.js
+++ server/boot/routes-settings.js
@@ -19,13 +19,25 @@ var debugVerbose = require('debug')('routes:verbose');
 module.exports = function (server) {
   var router = server.loopback.Router();
 
+
   // preferences and activity
   router.get('/settings', getCurrentUser(), ensureLoggedIn(), pendingFriendRequests(), function (req, res, next) {
     var ctx = req.myContext;
+    var isAdmin = false;
+    if (ctx.get('currentUserRoles')) {
+      var roles = ctx.get('currentUserRoles');
+      for (var i = 0; i < roles.length; i++) {
+        if (roles[i].role().name === 'admin') {
+          isAdmin = true;
+        }
+      }
+    }
+
     res.render('pages/settings', {
       'user': ctx.get('currentUser'),
       'globalSettings': ctx.get('globalSettings'),
-      'pendingFriendRequests': ctx.get('pendingFriendRequests')
+      'pendingFriendRequests': ctx.get('pendingFriendRequests'),
+      'isAdmin': isAdmin
     });
   });
 
