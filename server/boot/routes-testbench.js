0184489cd8020ac490488b78555f03d36274ad00
Michael Rhodes <mrhodes@mediapolis.com>
2018-03-17 16:21:55 -0400

    web sockets NewsFeedItem observer

--- server/boot/routes-testbench.js
+++ server/boot/routes-testbench.js
@@ -4,14 +4,23 @@ var watchFeed = require('../lib/watchFeed');
 module.exports = function (server) {
 	var router = server.loopback.Router();
 
-	// testbench
-	router.get('/testbench', getCurrentUser(), function (req, res, next) {
+	router.get('/status', getCurrentUser(), function (req, res, next) {
 		var ctx = req.myContext;
 
 		res.render('pages/testbench', {
 			'globalSettings': ctx.get('globalSettings'),
 			'currentUser': ctx.get('currentUser'),
 			'connections': watchFeed.connections
+		});
+	});
+
+	// testbench
+	router.get('/testbench', getCurrentUser(), function (req, res, next) {
+		var ctx = req.myContext;
+
+		res.render('pages/testbench', {
+			'globalSettings': ctx.get('globalSettings'),
+			'currentUser': ctx.get('currentUser')
 		});
 	});
 
