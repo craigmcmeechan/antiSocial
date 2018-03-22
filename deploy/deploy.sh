4d4f14fa20e4d4aef41cd75b7b79f20e9a95eefb
Michael Rhodes <mrhodes@mediapolis.com>
2018-03-22 16:30:55 -0400

    less lazy instantiation on frontend

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -55,9 +55,9 @@ deploy_eb () {
 
 deploy_ecs () {
 	if [ "$CIRCLE_BRANCH" == "production" ]; then
-		echo "deploying mr"
-		aws ecs update-service --cluster arn:aws:ecs:us-east-1:980978009426:cluster/antisocial-ae --service arn:aws:ecs:us-east-1:980978009426:service/antisocial-ae --force-new-deployment
 		echo "deploying ae"
+		aws ecs update-service --cluster arn:aws:ecs:us-east-1:980978009426:cluster/antisocial-ae --service arn:aws:ecs:us-east-1:980978009426:service/antisocial-ae --force-new-deployment
+		echo "deploying mr"
 		aws ecs update-service --cluster arn:aws:ecs:us-east-1:980978009426:cluster/antisocial-mr --service arn:aws:ecs:us-east-1:980978009426:service/antisocial-mr --force-new-deployment
 	fi
 	if [ "$CIRCLE_BRANCH" == "development" ]; then
