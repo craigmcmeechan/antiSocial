78549249ad1e7473df5883ccb6aa4aa0b6778e07
Michael Rhodes <mrhodes@mediapolis.com>
2018-04-05 13:06:54 -0400

    autopost cron  daemon
    refactor notifications fro routes-post into a reusable function from
    cron task

--- server/boot/01-rake.js
+++ server/boot/01-rake.js
@@ -1,6 +1,6 @@
 var async = require('async');
 var crc = require('crc');
-module.exports = function rake(server) {
+module.exports = function rake(server, updateDone) {
 	server.models.Settings.findOrCreate({
 		'where': {
 			'group': 'db_version_history'
@@ -11,23 +11,51 @@ module.exports = function rake(server) {
 			'version': 0
 		}
 	}, function (err, group) {
-		if (group.settings.version >= 1) {
-			return;
-		}
-
-		group.settings.version = 1;
-		group.save();
-
-		server.models.Friend.find({}, function (err, friends) {
-			async.map(friends, function (friend, cb) {
-				if (!friend.hash) {
-					friend.hash = crc.crc32(friend.remoteEndPoint).toString(16);
-					friend.save();
+		async.series([
+			function (done) {
+				if (group.settings.version >= 1) {
+					return async.setImmediate(function () {
+						done();
+					});
 				}
-				cb();
-			}, function (err) {
-				console.log('data upgrade complete');
-			});
+				server.models.Friend.find({}, function (err, friends) {
+					async.map(friends, function (friend, cb) {
+						if (!friend.hash) {
+							friend.hash = crc.crc32(friend.remoteEndPoint).toString(16);
+							friend.save();
+						}
+						cb();
+					}, function (err) {
+						console.log('data upgrade complete to v1');
+						group.settings.version = 1;
+						group.save();
+						done();
+					});
+				});
+			},
+			function (done) {
+				if (group.settings.version >= 2) {
+					return async.setImmediate(function () {
+						done();
+					});
+				}
+
+				server.models.Post.find({}, function (err, posts) {
+					async.map(posts, function (post, cb) {
+						post.posted = true;
+						post.save();
+						cb();
+					}, function (err) {
+						console.log('data upgrade complete to v2');
+						group.settings.version = 2;
+						group.save();
+						done();
+					});
+				});
+			}
+		], function (err) {
+			console.log('data upgrade complete');
+			updateDone();
 		});
 	});
 };
---
+++ server/lib/doPostNotifications.js
@@ -0,0 +1,171 @@
+var async = require('async');
+var uuid = require('uuid');
+var VError = require('verror').VError;
+var FB = require('fb');
+var resolvePostPhotos = require('./resolvePostPhotos');
+var server = require('../server')
+
+module.exports = function doPostNotifications(currentUser, post, done) {
+	var fbIdentity;
+	var twIdentity;
+	if (currentUser.identities()) {
+		for (var i = 0; i < currentUser.identities().length; i++) {
+			var identity = currentUser.identities()[i];
+			if (identity.provider === 'facebook') {
+				fbIdentity = identity;
+			}
+			if (identity.provider === 'twitter') {
+				twIdentity = identity;
+			}
+		}
+	}
+
+	async.waterfall([
+		function (cb) { // tell the world
+			if (!post.posted) {
+				return async.setImmediate(function () {
+					cb(null, null, post);
+				});
+			}
+
+			currentUser.pushNewsFeedItems.create({
+				'uuid': uuid(),
+				'type': 'post',
+				'source': server.locals.config.publicHost + '/' + currentUser.username,
+				'about': server.locals.config.publicHost + '/' + currentUser.username + '/post/' + post.uuid,
+				'target': post.about,
+				'visibility': post.visibility,
+				'details': {},
+				'tags': post.tags
+			}, function (err, news) {
+				if (err) {
+					var e = new VError(err, 'could push news feed');
+					return cb(e);
+				}
+				cb(null, news, post);
+			});
+		},
+		function makeNewsFeedItem(news, post, cb) { // notify self
+			if (!post.posted) {
+				return async.setImmediate(function () {
+					cb(null, post);
+				});
+			}
+
+			var item = {
+				'uuid': news.uuid,
+				'type': 'post',
+				'source': news.source,
+				'about': news.about,
+				'target': post.about,
+				'userId': currentUser.id,
+				'createdOn': news.createdOn,
+				'updatedOn': news.updatedOn,
+				'details': {},
+				'tags': post.tags
+			};
+
+			server.models.NewsFeedItem.create(item, function (err, item) {
+				if (err) {
+					var e = new VError(err, 'could not save NewsFeedItem');
+					return cb(e);
+				}
+
+				cb(err, post);
+			});
+		},
+		function crossPostFacebook(post, cb) {
+			if (!post.posted || !fbIdentity || post.visibility.indexOf('facebook') === -1) {
+				return async.setImmediate(function () {
+					cb(null, post);
+				});
+			}
+
+			resolvePostPhotos([post], function (err) {
+
+				var fb = new FB.Facebook({
+					'appId': process.env.FACEBOOK_CLIENT_ID,
+					'appSecret': process.env.FACEBOOK_CLIENT_SECRET,
+					'accessToken': fbIdentity.credentials.token
+				});
+
+				var body = '';
+				var links = [];
+
+				if (post.visibility.indexOf('public') !== -1) {
+					// pass the permilink of the post to facebook and let it build the preview
+					links = [server.locals.config.publicHost + '/' + currentUser.username + '/post/' + post.uuid];
+				}
+				else {
+					// try to adapt the post to facebook limitations
+					body = post.body;
+
+					// extract first link
+					links = post.body.match(/^(http[^\s\b]*)/gm);
+					if (links) {
+						body = body.replace(/^(http[^\s\b]*)/gm, '');
+					}
+
+					// strip all other markup
+					body = server.locals.marked(body);
+					body = body.replace(/<[^>]+>/g, '');
+
+					// if there are photos... attach the first one
+					if (post.sortedPhotos && post.sortedPhotos.length) {
+						var imageSet = post.sortedPhotos[0].uploads()[0].imageSet;
+						var image;
+						if (imageSet.large) {
+							image = imageSet.large.url;
+						}
+						else {
+							image = imageSet.original.url;
+						}
+						links = [image];
+					}
+				}
+
+				fb.api(
+					'/me/feed',
+					'POST', {
+						'message': body,
+						'link': links.length ? links[0] : null
+					},
+					function (response) {
+						if (response && response.error) {
+							var e = new VError(response.error, 'post saved but could cross post to facebook');
+							return cb(e);
+						}
+						cb(null, post);
+					}
+				);
+			});
+		}
+	], function (err, post) {
+		done(err, post);
+	});
+};
+
+/*
+server.models.Friend.findOne({
+	'where': {
+		'remoteEndPoint': friendEndPoint
+	}
+}, function (err, friend) {
+	currentUser.pushNewsFeedItems.create({
+		'uuid': uuid(),
+		'type': 'tag',
+		'source': server.locals.config.publicHost + '/' + currentUser.username,
+		'about': server.locals.config.publicHost + '/' + currentUser.username + '/post/' + post.uuid,
+		'target': friend.remoteEndPoint,
+		'visibility': post.visibility,
+		'details': {},
+		'tags': post.tags
+	}, function (err, news) {
+		if (err) {
+			var e = new VError(err, 'could push news feed');
+			return doneTag(e);
+		}
+		doneTag(null);
+	});
+});
+*/
