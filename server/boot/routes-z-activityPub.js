8f6b8f9b814b404c33a4b77bf1856e2fe0b3f8c2
Michael Rhodes <mrhodes@mediapolis.com>
2018-04-16 11:31:16 -0400

    #219

--- server/boot/routes-z-activityPub.js
+++ server/boot/routes-z-activityPub.js
@@ -4,11 +4,14 @@ var getFriendAccess = require('../middleware/context-getFriendAccess');
 module.exports = function (server) {
 	var router = server.loopback.Router();
 
-	var personRE = /^\/activity-pub\/([a-zA-Z0-9-]+)$/;
-	var outBoxRE = /^\/activity-pub\/([a-zA-Z0-9-]+)\/outbox$/;
-	var inBoxRE = /^\/activity-pub\/([a-zA-Z0-9-]+)\/inbox$/;
-	var followersRE = /^\/activity-pub\/([a-zA-Z0-9-]+)\/followers$/;
-	var followingRE = /^\/activity-pub\/([a-zA-Z0-9-]+)\/following$/;
+	var personRE = /^\/activitypubapi\/([a-zA-Z0-9-]+)$/;
+	var outBoxRE = /^\/activitypubapi\/([a-zA-Z0-9-]+)\/outbox$/;
+	var inBoxRE = /^\/activitypubapi\/([a-zA-Z0-9-]+)\/inbox$/;
+	var followersRE = /^\/activitypubapi\/([a-zA-Z0-9-]+)\/followers$/;
+	var followingRE = /^\/activitypubapi\/([a-zA-Z0-9-]+)\/following$/;
+	var likesRE = /^\/activitypubapi\/([a-zA-Z0-9-]+)\/likes$/;
+	var likedRE = /^\/activitypubapi\/([a-zA-Z0-9-]+)\/liked$/;
+	var sharesRE = /^\/activitypubapi\/([a-zA-Z0-9-]+)\/shares$/;
 
 	router.get(personRE, getCurrentUser(), getFriendAccess(), function (req, res, next) {
 		var ctx = req.myContext;
@@ -37,12 +40,14 @@ module.exports = function (server) {
 					'@language': 'en'
 				}],
 				'type': 'Person',
-				'id': server.locals.config.publicHost + '/activity-pub/' + user.username,
-				'following': server.locals.config.publicHost + '/activity-pub/' + user.username + '/following',
-				'followers': server.locals.config.publicHost + '/activity-pub/' + user.username + '/followers',
-				'liked': server.locals.config.publicHost + '/activity-pub/' + user.username + '/liked',
-				'inbox': server.locals.config.publicHost + '/activity-pub/' + user.username + '/inbox',
-				'outbox': server.locals.config.publicHost + '/activity-pub/' + user.username + '/outbox',
+				'id': server.locals.config.publicHost + '/activitypubapi/' + user.username,
+				'following': server.locals.config.publicHost + '/activitypubapi/' + user.username + '/following',
+				'followers': server.locals.config.publicHost + '/activitypubapi/' + user.username + '/followers',
+				'liked': server.locals.config.publicHost + '/activitypubapi/' + user.username + '/liked',
+				'likes': server.locals.config.publicHost + '/activitypubapi/' + user.username + '/likes',
+				'shares': server.locals.config.publicHost + '/activitypubapi/' + user.username + '/shares',
+				'inbox': server.locals.config.publicHost + '/activitypubapi/' + user.username + '/inbox',
+				'outbox': server.locals.config.publicHost + '/activitypubapi/' + user.username + '/outbox',
 				'name': user.name,
 				'icon': [
 					server.locals.getUploadForProperty('photo', user.uploads(), 'thumb', server.locals.config.publicHost + server.locals.headshotFPO).url
